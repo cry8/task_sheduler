a2b4619b60fdf81836808188e5efa582423e92b1
Dmytro Morozov <fraqable@gmail.com>
2016-04-29 00:30:45 +0300

    Added files via upload

---
+++ Main/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Model;
+using Presenter;
+using View;
+
+namespace Main
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            IView mview = new MView();
+            IModel model = new Model.Model();
+            Presenter.Presenter presenter = new Presenter.Presenter(mview, model);
+        }
+    }
+}
